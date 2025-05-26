f841f821870cdb536e961b6c3d6ac7c65bbd6db7
ihyeeun <oioigl@naver.com>
2025-05-26 19:36:10 +0900

    first commit

---
+++ src/main/java/com/uplus/together/TogetherApplication.java
@@ -0,0 +1,16 @@
+package com.uplus.together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetherApplication.class, args);
+	}
+
+}
+
+// swagger 주소
+// http://localhost:8080/swagger-ui/index.html
